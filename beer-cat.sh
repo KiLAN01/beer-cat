23540f3dc2590e55bb5944e89b088375bc9e18dd
KILANERROR404 <70495592+KILANERROR404@users.noreply.github.com>
2021-02-21 02:28:09 -0400

    Create beer-cat.sh

---
+++ beer-cat.sh
@@ -0,0 +1,43 @@
+#!bin/bash
+verde='\033[32m'
+branco='\033[37m'
+vermelho='\033[31m'
+azul='\033[34m'
+negro='\033[0;30m'
+rosa='\033[38;5;207m'
+amarelo='\033[33m'
+roxo='\033[35m'
+ciano='\033[1;36m'
+magenta='\033[1;35m'
+
+ echo -e "${vermelho}installing packages..."
+ sleep 8
+apt install figlet -y
+  apt install git -y
+apt install python -y
+apt install python2 -y
+sleep 6
+figlet ${amarelo}beer cat
+  echo -e "#${vermelho}responsible for the bad acts after installing the tool${vermelho}"
+   echo -e "${azul}developer <[user_invalid]>${magenta}"
+  echo -e "${amarelo} warning: if you are not downloaded it may be that the tool does not work${vermelho}"
+   echo -e "${azul}this tool installs tools"
+   
+    echo -e "${vermelho}starting to install the tools${magenta}"
+#installs ddos
+#tool quack 
+ echo -e "${amarelo}installs ddos"
+echo -e "${amarelo}[====================================]"
+ echo -e "${amarelo}(cloning RED_HAWK)"
+  echo -e "${amarelo}(RED_HAWK information)"
+  sleep 5
+ echo  -e "${amarelo}(analyze website for almost complete)"
+echo -e "${magenta}(====================================)"
+ git clone https://github.com/Tuhinshubhra/RED_HAWK
+echo -e "${magenta}(====================================)"
+echo -e "${amarelo}[====================================]"
+   echo -e "${amarelo}[cloning SocialSploit]"
+   echo -e "${amarelo}SocialSploit information"
+   echo -e "${amarelo}[phishing]"
+   echo -e "${amarelo}[====================================]"
+  echo -e  "${amarelo}"
