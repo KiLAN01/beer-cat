97cf9397202558bafbb8745bce5939b6e9f35ef1
KILANERROR404 <70495592+KILANERROR404@users.noreply.github.com>
2021-02-22 19:15:47 -0400

    Update beer-cat.sh

--- beer-cat.sh
+++ beer-cat.sh
@@ -1,37 +1,27 @@
 #!bin/bash
-verde='\033[32m'
-branco='\033[37m'
-vermelho='\033[31m'
-azul='\033[34m'
-negro='\033[0;30m'
-rosa='\033[38;5;207m'
 amarelo='\033[33m'
-roxo='\033[35m'
-ciano='\033[1;36m'
-magenta='\033[1;35m'
+branco='\033[0;37m' 
 
 sleep 6
-figlet ${amarelo}beer cat
-  echo -e "#${vermelho}responsible for the bad acts after installing the tool${vermelho}"
-   echo -e "${azul}developer <[user_invalid]>${magenta}"
-  echo -e "${amarelo} warning: if you are not downloaded it may be that the tool does not work${vermelho}"
-   echo -e "${azul}this tool installs tools"
+figlet ${amarelo}beer cat${branco}
+  echo -e "#${branco}responsible for the bad acts after installing the tool${vermelho}"
+   echo -e "${amarelo}developer <[user_invalid]>${magenta}"
+  echo -e "${branco} warning: if you are not downloaded it may be that the tool does not work${vermelho}"
+   echo -e "${amarelo}this tool installs tools"
    
-    echo -e "${vermelho}starting to install the tools${magenta}"
-#installs ddos
-#tool quack 
- echo -e "${amarelo}installs ddos"
-echo -e "${amarelo}[====================================]"
- echo -e "${amarelo}(cloning RED_HAWK)"
-  echo -e "${amarelo}(RED_HAWK information)"
+    echo -e "${branco}starting to install the tools${magenta}"
+echo -e "${branco}[====================================]"
+ echo -e "${branco}(cloning RED_HAWK)"
+  echo -e "${branco}(RED_HAWK information)"
   sleep 5
- echo  -e "${amarelo}(analyze website for almost complete)"
-echo -e "${magenta}(====================================)"
+ echo  -e "${branco}(analyze website for almost complete)"
+echo -e "${branco}(====================================)"
  git clone https://github.com/Tuhinshubhra/RED_HAWK
-echo -e "${magenta}(====================================)"
+echo -e "${branco}(====================================)"
+
 echo -e "${amarelo}[====================================]"
    echo -e "${amarelo}[cloning SocialSploit]"
-   echo -e "${amarelo}SocialSploit information"
+   echo -e "${amarelo}SocialSploit information:"
    echo -e "${amarelo}[phishing]"
    echo -e "${amarelo}[====================================]"
   echo -e  "${amarelo}installing..."
